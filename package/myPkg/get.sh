ad1dede8e1094c4840d4d57c374dd4925448117d
Your Name <you@example.com>
2022-02-22 23:20:50 -0800

    a

---
+++ package/myPkg/get.sh
@@ -0,0 +1,8 @@
+# app使用时间控制
+git clone https://github.com/destan19/OpenAppFilter.git 
+
+# 网易云
+git clone https://github.com/maxlicheng/luci-app-unblockmusic.git
+
+# kms
+git clone git@github.com:cokebar/luci-app-vlmcsd.git
