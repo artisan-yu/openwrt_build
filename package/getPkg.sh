16a51192f1ed3284d38654bbb499b9a85ecb4a99
Your Name <you@example.com>
2022-02-26 12:06:22 +0000

    a

--- package/getPkg.sh
+++ package/getPkg.sh
@@ -1,2 +1,3 @@
 git clone https://github.91chi.fun/https://github.com/BROBIRD/openwrt-r8168.git
 git clone https://github.91chi.fun/https://github.com/Leo-Jo-My/luci-theme-opentomcat.git
+git clone https://github.91chi.fun/https://github.com/maxlicheng/luci-app-unblockmusic.git
