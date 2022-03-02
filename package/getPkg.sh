e6f8ea5fd8d041d8f82162445b41ce72ca4dcea1
Your Name <you@example.com>
2022-03-02 04:50:05 +0000

    Wed Mar  2 04:50:05 UTC 2022

--- package/getPkg.sh
+++ package/getPkg.sh
@@ -1,4 +1,2 @@
 git clone https://github.91chi.fun/https://github.com/BROBIRD/openwrt-r8168.git
 git clone https://github.91chi.fun/https://github.com/destan19/OpenAppFilter.git
-git clone https://github.91chi.fun/https://github.com/lisaac/luci-lib-docker.git
-git clone https://github.91chi.fun/https://github.com/ayu-666/openwrt_docker.git
